b76438a0d0a8756fb050e25f99c394c44b024330
9750176669manosiranjeevi <53260296+9750176669manosiranjeevi@users.noreply.github.com>
2019-08-13 16:52:48 +0530

    Delete Loginform.php

--- Loginform.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<form action="details.php"method="post">
-username:<input type="text"name="uname"/><br><br>
-password:<input type="password"name="password"/><br><br>
-<input type="submit" value="show"/>
-</body>
-</html>
